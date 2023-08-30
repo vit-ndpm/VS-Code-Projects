30fb57e3c39b867fd3df99a9881d9443dd682bec
vit-ndpm <137673140+vit-ndpm@users.noreply.github.com>
2023-08-30 16:57:57 +0530

    Mapping

--- Livewire/LG Directory/LGDirectory/app/Http/Livewire/Mapping.php
+++ Livewire/LG Directory/LGDirectory/app/Http/Livewire/Mapping.php
@@ -2,12 +2,81 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class Mapping extends Component
 {
+    public $states;
+    public $districts;
+    public $tehsils;
+    public $subdistrictCode;
+    public $villageCode;
+   public $villageDetails;
+    public $villages;
+    public $stateCode;
+    public $districtCode;
+    public $localBodies;
+    public $localBodyCode;
+    public $wards;
+    public $wardCode;
+    public $blockCode;
+    public $blocks;
+    public $gps;
+    public $gpCode;
+    public $areaType;
+
     public function render()
     {
+
         return view('livewire.mapping');
+    }
+    function mount()
+    {
+        $this->states = Http::post('https://lgdirectory.gov.in/webservices/lgdws/stateList', ['verify' => false])->json();
+        // dd($this->states);
+
+    }
+    public function updatedstateCode($value)
+    {
+        //dd($value);
+        $this->districts = Http::post('https://lgdirectory.gov.in/webservices/lgdws/districtList?stateCode=' . $value, ['verify' => false])->json();
+        //dd($this->districts);
+    }
+    public function updateddistrictCode($value)
+    {
+        //dd($value);
+        $this->tehsils = Http::post('https://lgdirectory.gov.in/webservices/lgdws/subdistrictList?districtCode=' . $value, ['verify' => false])->json();
+        $this->localBodies = Http::post('https://lgdirectory.gov.in/webservices/lgdws/fetchUrbanLocalBodyForGivenDistrict?landRegionType=D&landRegionCode=' . $value, ['verify' => false])->json();
+        //d($this->tehsils);
+        $this->blocks = Http::post('https://lgdirectory.gov.in/webservices/lgdws/blockList?districtCode=' . $value, ['verify' => false])->json();
+
+    }
+    public function updatedsubdistrictCode($value)
+    {
+        //dd($value);
+        $this->villages = Http::post('https://lgdirectory.gov.in/webservices/lgdws/villageList?subDistrictCode=' . $value, ['verify' => false])->json();
+       // dd($this->villages);
+    }
+    public function updatedvillageCode($value)
+    {
+        //dd($value);
+        $this->villageDetails = Http::post('https://lgdirectory.gov.in/webservices/lgdws/villageHierarachyDetails?villageCode=' . $value, ['verify' => false])->json();
+        //dump($this->villageDetails);
+    }
+    public function updatedlocalBodyCode($value)
+    {
+        //dd($value);
+        $this->wards = Http::post('https://lgdirectory.gov.in/webservices/lgdws/lbwiseWardDetails?localbodyCode=' . $value, ['verify' => false])->json();
+        array_multisort(array_column($this->wards, 'wardCode'), SORT_ASC, $this->wards);        
+        //dump($this->villageDetails);
+    }
+    public function updatedblockCode($value)
+    {
+        //dd($value);
+        // array_multisort(array_column($this->blocks, 'wardCode'), SORT_ASC, $this->wards);        
+        //dump($this->villageDetails);
+                $this->gps = Http::post('https://lgdirectory.gov.in/webservices/lgdws/getBlockwiseMappedGP?blockCode=' . $value, ['verify' => false])->json();
+
     }
 }
--- Livewire/LG Directory/LGDirectory/resources/views/livewire/mapping.blade.php
+++ Livewire/LG Directory/LGDirectory/resources/views/livewire/mapping.blade.php
@@ -1,3 +1,248 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Mapping Section</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="inputGroupSelect01">State</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect01" wire:model='stateCode'>
+                                    <option selected>Choose your State</option>
+                                    @foreach ($states as $state)
+                                        <option value="{{ $state['stateCode'] }}">{{ $state['stateNameEnglish'] }}
+                                        </option>
+                                    @endforeach
+                                </select>
+
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="input-group mb-3"wire:ignore.self>
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="inputGroupSelect01">District</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect01" wire:model='districtCode'>
+                                    <option selected>Choose your District</option>
+                                    @isset($districts)
+                                        @foreach ($districts as $district)
+                                            <option value="{{ $district['districtCode'] }}">
+                                                {{ $district['districtNameEnglish'] }}({{ $district['districtNameLocal'] }})
+                                            </option>
+                                        @endforeach
+                                    @endisset
+                                </select>
+                            </div>
+
+                        </div>
+
+                        {{-- <div class="col-md-3">
+                            <div class="input-group mb-3"wire:ignore.self>
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="inputGroupSelect01">Tehsil</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect01" wire:model='subdistrictCode'>
+                                    <option selected>Choose your Tehsil</option>
+                                    @isset($tehsils)
+                                        @foreach ($tehsils as $tehsil)
+                                            <option value="{{ $tehsil['subdistrictCode'] }}">
+                                                {{ $tehsil['subdistrictNameEnglish'] }}({{ $tehsil['subdistrictNameLocal'] }})
+                                            </option>
+                                        @endforeach
+                                    @endisset
+                                </select>
+                            </div>
+
+                        </div> --}}
+                        <div class="col-md-1">
+                            <div class="row">
+                                <div class="form-check col-md-6">
+                                    <input class="form-check-input" type="radio" name="areaType" wire:model='areaType' value="0">
+                                    <label class="form-check-label" for="areaType">
+                                        Rural
+                                    </label>
+                                </div>
+                                <div class="form-check col-md-6">
+                                    <input class="form-check-input" type="radio" name="areaType" wire:model='areaType' value="1">
+                                    <label class="form-check-label" for="areaType">
+                                        Urban
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- <div class="col-md-3">
+                            <div class="input-group mb-3"wire:ignore.self>
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="inputGroupSelect01">Village</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect01" wire:model='villageCode'>
+                                    <option selected>Choose your Village</option>
+                                    @isset($villages)
+                                        @foreach ($villages as $village)
+                                            <option value="{{ $village['villageCode'] }}">
+                                                {{ $village['villageNameEnglish'] }}({{ $village['villageNameLocal'] }})
+                                            </option>
+                                        @endforeach
+                                    @endisset
+                                </select>
+                            </div>
+
+                        </div> --}}
+                    </div>
+                </div>
+
+
+                @if ($areaType == '0')
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    Rural Local Body
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+
+
+                                        <div class="col-md-3">
+                                            <div class="input-group mb-3"wire:ignore.self>
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text"
+                                                        for="inputGroupSelect01">Block</label>
+                                                </div>
+                                                <select class="custom-select" id="inputGroupSelect01"
+                                                    wire:model='blockCode'>
+                                                    <option selected>Choose your Block</option>
+                                                    @isset($blocks)
+                                                        @foreach ($blocks as $block)
+                                                            <option value="{{ $block['blockCode'] }}">
+                                                                {{ $block['blockNameEnglish'] }}
+                                                            </option>
+                                                        @endforeach
+                                                    @endisset
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="input-group mb-3"wire:ignore.self>
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text" for="inputGroupSelect01">Gram
+                                                        Panchayat</label>
+                                                </div>
+                                                <select class="custom-select" id="inputGroupSelect01"
+                                                    wire:model='localBodyCode'>
+                                                    <option selected>Choose Gram Panchayat</option>
+                                                    @isset($gps)
+                                                        @foreach ($gps as $gp)
+                                                            <option value="{{ $gp['localBodyCode'] }}">
+                                                                {{ $gp['localBodyNameEnglish'] }}({{ $gp['localBodyCode'] }})
+                                                            </option>
+                                                        @endforeach
+                                                    @endisset
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    Urban Local Body
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+
+
+                                        <div class="col-md-3">
+                                            <div class="input-group mb-3"wire:ignore.self>
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text" for="inputGroupSelect01">Local
+                                                        Body</label>
+                                                </div>
+                                                <select class="custom-select" id="inputGroupSelect01"
+                                                    wire:model='localBodyCode'>
+                                                    <option selected>Choose your Local Body</option>
+                                                    @isset($localBodies)
+                                                        @foreach ($localBodies as $localBody)
+                                                            <option value="{{ $localBody['localBodyCode'] }}">
+                                                                {{ $localBody['localBodyNameEnglish'] }}
+                                                            </option>
+                                                        @endforeach
+                                                    @endisset
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-3">
+                                            <div class="input-group mb-3"wire:ignore.self>
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text"
+                                                        for="inputGroupSelect01">Ward</label>
+                                                </div>
+                                                <select class="custom-select" id="inputGroupSelect01"
+                                                    wire:model='wardCode'>
+                                                    <option selected>Choose your Ward</option>
+                                                    @isset($wards)
+                                                        @foreach ($wards as $ward)
+                                                            <option value="{{ $ward['wardCode'] }}">
+                                                                {{ $ward['wardNumber'] }}-{{ $ward['wardNameEnglish'] }}
+                                                            </option>
+                                                        @endforeach
+                                                    @endisset
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @isset($villageDetails)
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card">
+                                <div class="card-header">
+                                    Detail of Selected Village
+                                </div>
+                                <div class="card-body">
+                                    <p>
+                                        State: {{ $villageDetails['stateMini']['stateNameEnglish'] }}(
+                                        {{ $villageDetails['stateMini']['stateCode'] }})
+                                    </p>
+                                    <p>
+                                        District: {{ $villageDetails['districtMini']['districtNameEnglish'] }}(
+                                        {{ $villageDetails['districtMini']['districtCode'] }})
+                                    </p>
+                                    <p>
+                                        Tehsil: {{ $villageDetails['subdistrictMini']['subdistrictNameEnglish'] }}(
+                                        {{ $villageDetails['subdistrictMini']['subdistrictCode'] }})
+                                    </p>
+                                    <p>
+                                        Block: {{ $villageDetails['blockMini']['blockNameEnglish'] }}(
+                                        {{ $villageDetails['blockMini']['blockCode'] }})
+                                    </p>
+                                    <p>
+                                        Local Body: {{ $villageDetails['localbodyMiniList'][0]['localbodyNameEnglish'] }}(
+                                        {{ $villageDetails['localbodyMiniList'][0]['localbodyCode'] }})
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endisset
+            </div>
+        </div>
+    </div>
 </div>
